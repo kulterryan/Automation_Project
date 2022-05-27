4cae67b7849accfd96d719c1102bb6eb5161097d
kulterryan <chaurasia.aryan@yahoo.com>
2022-05-27 19:49:42 +0530

    Updated

--- Automation.sh
+++ Automation.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 MYNAME="aryan"
+S3_BUCKET="upgrad-aryan"
 # Execute APT Update
 echo "Executing APT Update"
-#sudo apt update -y
+sudo apt update -y
 
 # Checking Apache2 Installation
 INSTALLED=$(dpkg --get-selections apache2 | awk '{print $1}')
@@ -13,8 +14,8 @@ if [[ $INSTALLED == apache2 ]]; then
     echo "Apache2 is already installed"
 else
     echo "Apache2 is not installed"
-#    sudo apt update -y
-#    sudo apt install apache2 -y
+    sudo apt update -y
+    sudo apt install apache2 -y
 fi
 
 # Checking Apache System Status
@@ -25,7 +26,9 @@ if [[ $RUNAPACHE == "s" ]]; then
 else
     # Running Apache2 Service
     echo "Apache2 is not running"
+    echo "Starting Apache2 Server"
     sudo systemctl start apache2
+    echo "Apache2 is started"
 fi
 
 # Timestamp
@@ -35,4 +38,10 @@ FILENAME="/tmp/${MYNAME}-httpd-logs-${TIMESTAMP}.tar"
 # Creating TAR File
 echo "Creating Apache2 Log Archives"
 
-tar -cf $(tar -cf ${FILENAME} $(find /var/log/apache2/ -name "*.log") 2>&1)
+tar -cf ${FILENAME} $(find /var/log/apache2/ -name "*.log") 2>&1
+
+# Copy to S3 Bucket
+echo "Copying File to S3 Bucket"
+aws s3 cp ${FILENAME} s3://${S3_BUCKET}/${FILENAME}
+echo "Uploading Completed..."
+echo "Successfully Uploaded ${FILENAME} to S3 Bucket"
